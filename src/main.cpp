9f2a684e9bc3bf8c22b02949a4f113a21d9d2a92
Lukasz <setlaklukasz@gmail.com>
2026-06-06 19:14:48 +0200

    Measure the CPU execution time

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include "../include/perlin.h"
+
+#include <chrono>
 #include <iostream>
 
 int main()
 {
+    constexpr int width = 1024;
+    constexpr int height = 1024;
+
+    constexpr float scale = 0.02f;
+
+    volatile float sum = 0.0f;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            sum += perlin(x * scale, y * scale);
+        }
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::chrono::duration<double> elapsed = end - start;
+
+    std::cout << "CPU execution time: "
+        << elapsed.count()
+        << " s\n";
+
     return 0;
 }

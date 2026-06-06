d3ad4dd858c1aad58bc620bba063e7fffaf88ae5
Lukasz <setlaklukasz@gmail.com>
2026-06-06 19:16:36 +0200

    Add file generation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../include/perlin.h"
 
 #include <chrono>
+#include <fstream>
 #include <iostream>
 
 int main()
@@ -10,16 +11,31 @@ int main()
 
     constexpr float scale = 0.02f;
 
-    volatile float sum = 0.0f;
-
     auto start = std::chrono::high_resolution_clock::now();
+
+    std::ofstream file("output/noise_cpu.pgm");
+
+    file << "P2\n";
+    file << width << " " << height << "\n";
+    file << "255\n";
 
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
         {
-            sum += perlin(x * scale, y * scale);
+            float value = perlin(x * scale, y * scale);
+
+            value = (value + 1.0f) * 0.5f;
+
+            int gray = static_cast<int>(value * 255.0f);
+
+            if (gray < 0) gray = 0;
+            if (gray > 255) gray = 255;
+
+            file << gray << ' ';
         }
+
+        file << '\n';
     }
 
     auto end = std::chrono::high_resolution_clock::now();

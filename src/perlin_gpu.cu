2d6d5996cde554063447878f3b1edf427eeb98ea
Lukasz <setlaklukasz@gmail.com>
2026-06-06 19:55:44 +0200

    Implement Perlin noise function on GPU

--- src/perlin_gpu.cu
+++ src/perlin_gpu.cu
@@ -9,3 +9,53 @@
 #define HEIGHT 1024
 
 __device__ __constant__ int d_permutation[512];
+
+__device__ float fade(float t)
+{
+    return t * t * t * (t * (t * 6 - 15) + 10);
+}
+
+__device__ float lerp(float a, float b, float t)
+{
+    return a + t * (b - a);
+}
+
+__device__ float grad(int hash, float x, float y)
+{
+    int h = hash & 7;
+    float u = h < 4 ? x : y;
+    float v = h < 4 ? y : x;
+
+    return ((h & 1) ? -u : u) + ((h & 2) ? -v : v);
+}
+
+__device__ float perlin_device(float x, float y)
+{
+    int X = ((int)floorf(x)) & 255;
+    int Y = ((int)floorf(y)) & 255;
+
+    x -= floorf(x);
+    y -= floorf(y);
+
+    float u = fade(x);
+    float v = fade(y);
+
+    int aa = d_permutation[d_permutation[X] + Y];
+    int ab = d_permutation[d_permutation[X] + Y + 1];
+    int ba = d_permutation[d_permutation[X + 1] + Y];
+    int bb = d_permutation[d_permutation[X + 1] + Y + 1];
+
+    float result =
+        lerp(
+            lerp(
+                grad(aa, x, y),
+                grad(ba, x - 1.0f, y),
+                u),
+            lerp(
+                grad(ab, x, y - 1.0f),
+                grad(bb, x - 1.0f, y - 1.0f),
+                u),
+            v);
+
+    return result;
+}

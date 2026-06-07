f23224e35895d5ac83ab635e564e122b5c99ae2c
Lukasz <setlaklukasz@gmail.com>
2026-06-08 00:26:26 +0200

    Correct separators

--- src/csv_writer.cpp
+++ src/csv_writer.cpp
@@ -1,32 +1,26 @@
 #include "../include/csv_writer.h"
 #include <fstream>
 #include <filesystem>
+#include <iomanip>
 
 using namespace std;
 
 void createBenchmarkCsv(const string& filename)
 {
-    if (filesystem::exists(filename))
-        return;
-
     ofstream file(filename);
 
-    file << "width,height,implementation,iteration,time\n";
+    file << "width;height;implementation;iteration;time\n";
 }
 
-void appendBenchmarkResult(
-    const string& filename,
-    int width,
-    int height,
-    const string& implementation,
-    int iteration,
-    double time)
+void appendBenchmarkResult(const string& filename, int width, int height, const string& implementation,int iteration, double time)
 {
     ofstream file(filename, ios::app);
 
-    file << width << ","
-        << height << ","
-        << implementation << ","
-        << iteration << ","
+    file << fixed << setprecision(9);
+
+    file << width << ";"
+        << height << ";"
+        << implementation << ";"
+        << iteration << ";"
         << time << "\n";
 }

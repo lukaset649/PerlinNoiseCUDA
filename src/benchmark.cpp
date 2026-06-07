a61cb8267ae44e4ae5059387bc4528e727cac017
Lukasz <setlaklukasz@gmail.com>
2026-06-07 21:47:20 +0200

    Add file saving to benchmark

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,25 +1,32 @@
 #include "../include/benchmark.h"
 #include "../include/perlin.h"
+#include "../include/image_io.h"
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
-void runBenchmark(const std::vector<Resolution>& resolutions, float scale, int octaves, float persistence, float lacunarity)
+void runBenchmark(const vector<Resolution>& resolutions, float scale, int octaves, float persistence, float lacunarity)
 {
-    std::cout << std::fixed << std::setprecision(6);
+    cout << fixed << setprecision(6);
 
     for (const auto& res : resolutions)
     {
-        std::vector<unsigned char> imageCPU(res.width * res.height);
-        std::vector<unsigned char> imageGPU(res.width * res.height);
+        vector<unsigned char> imageCPU(res.width * res.height);
+        vector<unsigned char> imageGPU(res.width * res.height);
 
         double cpuTime = generateNoiseCPU(imageCPU.data(), res.width, res.height, scale, 5, 0.5f, 2.0f);
         double gpuTime = generateNoiseGPU(imageGPU.data(), res.width, res.height, scale, 5, 0.5f, 2.0f);
 
+        string cpuFilename = "output/" + to_string(res.width) + "x" + to_string(res.height) + "_cpu.pgm";
+        string gpuFilename = "output/" + to_string(res.width) + "x" + to_string(res.height) + "_gpu.pgm";
 
-        cout << "Resolution: "<< res.width << "x" << res.height << '\n';
+        savePGM(cpuFilename.c_str(), imageCPU.data(), res.width, res.height);
+        savePGM(gpuFilename.c_str(), imageGPU.data(), res.width, res.height);
+
+        cout << "Resolution: "<< res.width << "x" << res.height << '\n' << '\n';
         cout << "CPU time: "<< cpuTime << " s\n";
         cout << "GPU time: "<< gpuTime << " s\n";
         cout << "-----------------------------\n";
